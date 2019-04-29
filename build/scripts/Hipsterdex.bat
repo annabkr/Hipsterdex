52fcb2c08e08c4e5471179d6095b44b8329337c0
hbaker <hbaker@cs.odu.edu>
2019-04-29 14:47:34 +0800

    Expanded unit tests

--- build/scripts/Hipsterdex.bat
+++ build/scripts/Hipsterdex.bat
@@ -66,7 +66,7 @@ set CMD_LINE_ARGS=%*
 set CLASSPATH=%APP_HOME%\lib\Hipsterdex.jar;%APP_HOME%\lib\boilerpipe-1.2.2.jar;%APP_HOME%\lib\guava-23.0.jar;%APP_HOME%\lib\nekohtml-1.9.19.jar;%APP_HOME%\lib\xercesImpl-2.11.0.jar;%APP_HOME%\lib\jsr305-1.3.9.jar;%APP_HOME%\lib\error_prone_annotations-2.0.18.jar;%APP_HOME%\lib\j2objc-annotations-1.1.jar;%APP_HOME%\lib\animal-sniffer-annotations-1.14.jar;%APP_HOME%\lib\xml-apis-1.4.01.jar
 
 @rem Execute Hipsterdex
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %HIPSTERDEX_OPTS%  -classpath "%CLASSPATH%" hipsterdex.Hipsterdex %CMD_LINE_ARGS%
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %HIPSTERDEX_OPTS%  -classpath "%CLASSPATH%" com.ab.hipsterdex.Hipsterdex %CMD_LINE_ARGS%
 
 :end
 @rem End local scope for the variables with windows NT shell
